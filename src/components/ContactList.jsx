19d27589560bbee1c4eb548d8d267ad51a67738b
Andrii2024 <krovlya2005@gmail.com>
2024-03-03 19:42:58 +0200

    up

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { StyledButtonList, StyledLi } from './App.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   deleteContact,
   selectContacts,
 } from '../redux/phonebook/contactsSlice';
+import { selectFilter } from '../redux/phonebook/filtersSlice';
+import { Notification } from './Notific';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
+
+  const filteredContacts = useMemo(() => {
+    if (filter === '') return contacts;
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }, [contacts, filter]);
 
   return (
-    <ul>
-      {contacts.map(contact => (
-        <StyledLi key={contact.id}>
-          {contact.name} - {contact.number}
-          <StyledButtonList onClick={() => dispatch(deleteContact(contact.id))}>
-            Delete
-          </StyledButtonList>
-        </StyledLi>
-      ))}
-    </ul>
+    <>
+      {filteredContacts?.length === 0 ? (
+        <Notification message="No contacts have been found" />
+      ) : (
+        <ul>
+          {contacts.map(contact => (
+            <StyledLi key={contact.id}>
+              {contact.name} - {contact.number}
+              <StyledButtonList
+                onClick={() => dispatch(deleteContact(contact.id))}
+              >
+                Delete
+              </StyledButtonList>
+            </StyledLi>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
